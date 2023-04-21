53e6a897ed5a249920a753b5f3c07cc7163e643e
JellyApple102 <jcarryer@outlook.com>
2023-04-21 11:49:00 -0400

    lots more misc

--- waybar/.config/waybar/custom/kb-backlight.sh
+++ waybar/.config/waybar/custom/kb-backlight.sh
@@ -4,6 +4,9 @@ while true
 do
     percentage=`brightnessctl -d kbd_backlight info | grep -o -E '\((.+%)\)'`
     percentage=${percentage:1:-2}
-    echo $percentage
+
+    json='{"percentage":'"$percentage"', "tooltip":"Keyboard Backlight: '"$percentage"'%"}'
+    echo $json
+
     sleep 0.2
 done
--- waybar/.config/waybar/custom/screen-brightness.sh
+++ waybar/.config/waybar/custom/screen-brightness.sh
@@ -4,6 +4,9 @@ while true
 do
     percentage=`brightnessctl info | grep -o -E '\((.+%)\)'`
     percentage=${percentage:1:-2}
-    echo $percentage
+
+    json='{"percentage":'"$percentage"', "tooltip":"Screen Brightness: '"$percentage"'%"}'
+    echo $json
+
     sleep 0.2
 done
